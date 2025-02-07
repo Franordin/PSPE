417cc81f60cf684c74f92a83da902d9ebbc35382
Franordin <98395260+Franordin@users.noreply.github.com>
2025-02-08 05:16:13 +0900

    Draw constraints as lines

--- PSPE/PSPE/Constraint.h
+++ PSPE/PSPE/Constraint.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#ifndef CONSTRAINT_H
+#define CONSTRAINT_H
+
+#include "Particle.h"
+#include <cmath>
+#include <limits>
+
+class Constraint {
+public:
+	Particle* p1 = new Particle(1.0f, 1.0f);
+	Particle* p2 = new Particle(1.0f, 1.0f);
+	float initial_length;
+
+	Constraint(Particle* p1, Particle* p2)
+		: p1(p1), p2(p2),
+		initial_length(std::hypot(p2->position.x - p1->position.x,
+			p2->position.y - p1->position.y))
+	{}
+
+	/// Constraint Satisfaction - Maintain distance between two points
+	void satisfy() {
+		sf::Vector2f delta = p2->position - p1->position;
+		float current_length = std::hypot(delta.x, delta.y);
+		float difference = (current_length - initial_length) / current_length;
+		sf::Vector2f correction = delta * 0.5f * difference;
+
+		p1->position += correction;
+		p2->position -= correction;
+	}
+};
+
+#endif // CONSTRAINT_H
--- PSPE/PSPE/main.cpp
+++ PSPE/PSPE/main.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
-#include "particle.h"
+#include "Particle.h"
+#include "Constraint.h"
 
 const int WIDTH = 1080;
 const int HEIGHT = 640;
@@ -20,6 +21,18 @@ int main() {
 	particles.emplace_back(WIDTH / 2 - 50, HEIGHT / 2 + 50);
 	particles.emplace_back(WIDTH / 2 - 50, HEIGHT / 2 - 50);
 
+	std::vector<Constraint> constraints;
+	constraints.emplace_back(&particles[0], &particles[1]);
+	constraints.emplace_back(&particles[0], &particles[2]);
+	constraints.emplace_back(&particles[0], &particles[3]);
+	constraints.emplace_back(&particles[0], &particles[4]);
+	constraints.emplace_back(&particles[1], &particles[2]);
+	constraints.emplace_back(&particles[1], &particles[3]);
+	constraints.emplace_back(&particles[1], &particles[4]);
+	constraints.emplace_back(&particles[2], &particles[3]);
+	constraints.emplace_back(&particles[2], &particles[4]);
+	constraints.emplace_back(&particles[3], &particles[4]);
+
 	while (window.isOpen()) {
 		sf::Event event;
 
@@ -27,22 +40,36 @@ int main() {
 			if (event.type == sf::Event::Closed)
 				window.close();
 
-		// Apply gravity and update particles
+		/// Apply gravity and update particles
 		for (auto& particle : particles) {
 			particle.apply_force(sf::Vector2f(0, GRAVITY));
 			particle.update(TIME_STEP);
 			particle.constraint_to_bounds(WIDTH, HEIGHT, PARTICLE_RADIOUS);
 		}
 
+		for (size_t i = 0; i < 5; i++)
+			for (auto& constraint : constraints)
+				constraint.satisfy();
+
 		window.clear(sf::Color::Black);
 
-		// Draw particles as balls
+		/// Draw particles as balls
 		for (const auto& particle : particles) {
 			sf::CircleShape circle(PARTICLE_RADIOUS);
 			circle.setFillColor(sf::Color::White);
 			circle.setPosition(particle.position.x - PARTICLE_RADIOUS,
 								particle.position.y - PARTICLE_RADIOUS);
 			window.draw(circle);
+		}
+
+		/// Draw constraints as lines
+		for (const auto& constraint : constraints) {
+			sf::Vertex line[] = {
+				sf::Vertex(constraint.p1->position, sf::Color::Green),
+				sf::Vertex(constraint.p2->position, sf::Color::Green),
+			};
+
+			window.draw(line, 2, sf::Lines);
 		}
 
 		window.display();
